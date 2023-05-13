88ba7274cc9d788c2a08a03a530754d66fcdc9f0
typeperfest <perfect2111@gmail.com>
2023-05-13 16:49:35 +0300

    initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <getopt.h>
+
+typedef unsigned char byte_t;
+
+int main(int argc, char* argv[]) {
+    byte_t key = 0;
+    char* inFileName = NULL;
+    char* outFileName = NULL;
+    
+    return 0;
+}
