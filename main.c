1c47373b59c8c87a4001c57022705cbd94878e50
typeperfest <perfect2111@gmail.com>
2023-05-13 17:46:50 +0300

    added cli parsing

--- main.c
+++ main.c
@@ -1,4 +1,8 @@
 #include <getopt.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
 
 typedef unsigned char byte_t;
 
@@ -6,8 +10,38 @@ int main(int argc, char* argv[]) {
     byte_t key = 0;
     char* inFileName = NULL;
     char* outFileName = NULL;
-    
-    
+    int c;
 
-    return 0;
+    opterr = 0;
+
+    while ((c = getopt(argc, argv, "k:i:o:")) != -1) {
+        switch (c) {
+            case 'k':
+                key = atoi(optarg);
+                break;
+            case 'i':
+                inFileName = optarg;
+                break;
+            case 'o':
+                outFileName = optarg;
+                break;
+            case '?':
+                if (optopt == 'k' || optopt == 'i' || optopt == 'o') {
+                    fprintf(stderr, "Option -%c reqieres an argument.\n Aborting...", optopt);
+                } else if (isprint(optopt)) {
+                    fprintf(stderr, "Unknown option -%c. Aborting...\n", optopt);
+                } else {
+                    fprintf(stderr, "Unknown option characher. Aborting...");
+                }
+                exit(EXIT_FAILURE);
+            default:
+                abort();
+        }
+    }
+
+    printf("Read key is %d\n", key);
+    printf("Read input file is %s\n", inFileName);
+    printf("Read output file is %s\n", outFileName);
+
+    exit(EXIT_SUCCESS);
 }
