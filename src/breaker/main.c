d2904d74c9a072fff6dd6c4d41b0defbc48f3e2a
typeperfest <perfect2111@gmail.com>
2023-05-24 19:10:18 +0300

    remove debug printf's

--- src/breaker/main.c
+++ src/breaker/main.c
@@ -20,44 +20,6 @@ int main() {
         fscanf(dictf, " %31s ", wordBuff);
         dictAddWord(&dictionary, wordBuff);
     }
-    int a = dictContains(&dictionary, "123");
-    int b = dictContains(&dictionary, "fffff");
-    int c = dictContains(&dictionary, "aaa");
-    int d = dictContains(&dictionary, "3123");
-    int e = dictContains(&dictionary, "fa");
-    int f = dictContains(&dictionary, "tg");
-    int g = dictContains(&dictionary, "kkl");
-    int h = dictContains(&dictionary, "k");
-    int i = dictContains(&dictionary, "qre");
-
-    int af = dictContains(&dictionary, "4314");
-    int bf = dictContains(&dictionary, "4ds");
-    int cf = dictContains(&dictionary, "aaaf");
-    int df = dictContains(&dictionary, "df");
-    int ef = dictContains(&dictionary, "rrt");
-    int ff = dictContains(&dictionary, "bvbc");
-    int gf = dictContains(&dictionary, "54sdf");
-    int hf = dictContains(&dictionary, "nfdg");
-    int fi = dictContains(&dictionary, "oo1");
-    printf("a = %d\n", a);
-    printf("b = %d\n", b);
-    printf("c = %d\n", c);
-    printf("d = %d\n", d);
-    printf("e = %d\n", e);
-    printf("f = %d\n", f);
-    printf("g = %d\n", g);
-    printf("h = %d\n", h);
-    printf("i = %d\n", i);
-
-    printf("af = %d\n", af);
-    printf("bf = %d\n", bf);
-    printf("cf = %d\n", cf);
-    printf("df = %d\n", df);
-    printf("ef = %d\n", ef);
-    printf("ff = %d\n", ff);
-    printf("gf = %d\n", gf);
-    printf("hf = %d\n", hf);
-    printf("fi = %d\n", fi);
 
     clearDict(&dictionary);
     return 0;
