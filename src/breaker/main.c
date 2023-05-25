488613a6feb47be7268c279cdd4501cc5ddb95b9
typeperfest <perfect2111@gmail.com>
2023-05-25 21:47:29 +0300

    finish breaker implementation

--- src/breaker/main.c
+++ src/breaker/main.c
@@ -1,12 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
 
 #include "xorist-breaker.h"
 
 #define MAX_WORD_SIZE 32
 
-int main() {
-    FILE* dictf = fopen("dictionary.txt", "r");
+int main(int argc, char* argv[]) {
+    const char* dictfname = NULL;
+    const char* crackfname = NULL;
+    int c = 0;
+
+    if (argc != 5) {
+        printHelp();
+        exit(EXIT_FAILURE);
+    }
+
+    while ((c = getopt(argc, argv, "d:c:")) != -1 ) {
+        switch(c) {
+            case 'd':
+                dictfname = strdup(optarg);
+                break;
+            case 'c':
+                crackfname = strdup(optarg);
+                break;
+            default:
+                printHelp();
+                exit(EXIT_FAILURE);
+                break;
+        }
+    }
+
+    FILE* dictf = fopen(dictfname, "r");
     printf("dictionary.txt should contain words less than 32 characnters in size and separated by \\n symbol.\n");
     if (!dictf) {
         fprintf(stderr, "File dictionaty.txt not found. Aborting...");
@@ -21,15 +47,26 @@ int main() {
         dictAddWord(&dictionary, wordBuff);
     }
     fclose(dictf);
-    /*
-    TODO: read cracking file
-    TODO: if cycle [0..255] body: make key as i,
-                            apply key to a text
-                            check if file contains spaces, 
-                            if not - continue, else check word in dictionary,
-                            if word founded - out as possible plaintext
-                            else continue
-    */
+
+    FILE* crackf = fopen(crackfname, "r");
+    size_t fsize = getFileSize(crackf);
+    const char* textBuffer = allocateBuffer(fsize + 1);
+    readFileIntoBuffer(textBuffer, fsize, crackf);
+
+    for (int key = 0; key < 256; ++key) {
+        char* tmpBuf = strdup(textBuffer);
+        xorBuffer(tmpBuf, fsize, key);
+        if (getSpaces(tmpBuf, fsize)) {
+            if (checkWordsInbuffer(tmpBuf, fsize, &dictionary)) {
+                printf("=== MATCHES FOUNDED ===\n");
+                printf("%s", tmpBuf);
+                printf("=======================\n");
+            }
+        }
+        free(tmpBuf);
+    }
+    free(textBuffer);
     clearDict(&dictionary);
+    fclose(crackf);
     return 0;
 }
--- src/breaker/xorist-breaker.h
+++ src/breaker/xorist-breaker.h
@@ -11,6 +11,7 @@ typedef struct {
     size_t _reserved_size;
 } dictionary_t;
 
+void printHelp();
 size_t computeHash(const char* argument, const size_t modulo);
 void allocateDictionary(dictionary_t* dict, const size_t size);
 void dictAddWord(dictionary_t* const dict, const char* word);
@@ -18,5 +19,11 @@ int dictContains(dictionary_t* const dict, const char* word);
 size_t getLinesCount(FILE* stream);
 void clearDict(dictionary_t* dict);
 void moveNextLine(FILE* stream);
+size_t getFileSize(FILE* filePtr);
+char* allocateBuffer(size_t bufSize);
+size_t readFileIntoBuffer(char* buffer, size_t size, FILE* filePtr);
+void xorBuffer(char* buffer, size_t size, unsigned char key);
+int getSpaces(char* buffer, size_t size);
+int checkWordsInbuffer(char* buffer, size_t size, dictionary_t* const dict);
 
 #endif // BREAKER_H_
